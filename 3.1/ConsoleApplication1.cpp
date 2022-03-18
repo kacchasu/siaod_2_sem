a0adbf3a5e3b2bc61d50bb63e71ab38ea39b3436
kacchasu <7.germania.7@gmail.com>
2022-03-18 21:44:06 +0300

    3 nomer

---
+++ 3.1/ConsoleApplication1.cpp
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+using namespace std;
+void redactchar(char * si)
+{
+    char s2[] = " ,.!";
+    char* s = strtok(si, s2);
+    char st[1000] = "";
+    strcpy(st, s);
+    char begins[1000] = "";
+    strcat(begins, st);
+    char ends[1000] = "";
+    while (s != NULL)//пока не достигнем конца строки (\0 или NULL)
+    {
+        bool ok = true;
+        s = strtok(NULL, s2);	//указатель на слово между \0 и символом разделителем
+        if (s != NULL) {
+            for (int i = 0; i < strlen(s); i++)
+                if (s[i] != '\0' and strchr(st, s[i]) == 0)
+                    ok = false;
+            if (ok) {
+                strcat(begins, " ");
+                strcat(begins, s);
+            }
+            else {
+                strcat(ends, " ");
+                strcat(ends, s);
+            }
+        }
+    }
+    strcat(begins, ends);
+    cout << begins;
+}
+int main()
+{
+    setlocale(0, "");
+    char si[1000];
+    cout << "Enter string: ";
+    gets_s(si);  
+    redactchar(si);
+}
---
+++ 3.2/3.2.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+using namespace std;
+void redactchar(string s)
+{
+    string loc = " ,";
+    string st = s.substr(0, s.find_first_of(loc));
+    s.erase(0, s.find_first_of(loc) + 1);
+    string ans = st;
+    string ends = "";
+    while (s.size() != 0) {
+        bool ok = true;
+        string temp = s.substr(0, s.find_first_of(loc));
+        s.erase(0, s.find_first_of(loc) + 1);
+        for (int i = 0; i < temp.size(); i++)
+            if (st.find(temp[i]) == string::npos)
+                ok = false;
+        if (ok)
+            ans += ' ' + temp;
+        else
+            ends += ' ' + temp;
+    }
+    ans += ends;
+    cout << ans << endl;
+}
+int main()
+{
+    string s;
+    cout << "Enter string: ";
+    getline(cin, s);
+    s += ' ';
+    redactchar(s);
+}
