1e261cb91f290785f912848e8e3e81d89decea83
kacchasu <7.germania.7@gmail.com>
2022-03-28 16:13:16 +0300

    6 nomer

---
+++ 6.1/6.1.cpp
@@ -0,0 +1,31 @@
+﻿// 6.1.cpp : 1.	Проверить баланс скобок в арифметическом выражении, которое передано как строка.
+
+#include <iostream>
+#include <string>
+#include <fstream> 
+#include <algorithm>
+using namespace std;
+static const string kinds = "()";
+bool checker(string s, int counter = 0)
+{
+    if (counter < 0) return false;
+    for (int i = 0; i < s.size(); i++)
+        switch (s[i])
+        {
+        case '(': return checker(s.substr(1), counter + 1);
+        case ')': return checker(s.substr(1), counter - 1);
+        default: return checker(s.substr(1), counter);
+        }
+    return (counter == 0);
+}
+
+int main()
+{
+    string staple;
+    cout << "Enter expression: ";
+    getline(cin, staple);
+    if (checker(staple))
+        cout << "staples in expression: " << staple << " is balanced";
+    else
+        cout << "staples in expression: " << staple << " is not balanced";
+}
---
+++ 6.2/6.2.cpp
@@ -0,0 +1,81 @@
+﻿// 6.2.cpp : 2.	Найти минимальное значение в однонаправленном списке
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+using namespace std;
+struct Node
+{
+    int val;
+    Node* next;
+    Node(int _val) : val(_val), next(nullptr) {};
+};
+struct list
+{
+    Node* first;
+    Node* last;
+    list() : first(nullptr), last(nullptr) {};
+    bool is_empty()
+    {
+        return first == nullptr;
+    }
+    void push_back(int _val)
+    {
+        Node* p = new Node(_val);
+        if (is_empty()) {
+            first = p;
+            last = p;
+            return;
+        }
+        last->next = p;
+        last = p;
+    }
+    void print()
+    {
+        cout << "Your linked list: ";
+        if (is_empty()) return;
+        Node* p = first;
+        while (p) {
+            cout << p->val << " ";
+            p = p->next;
+        }
+        cout << endl;
+    }
+    Node* operator[] (const int index)
+    {
+        if (is_empty()) return nullptr;
+        Node* p = first;
+        for (int i = 0; i < index; i++) {
+            p = p->next;
+            if (!p) return nullptr;
+        }
+        return p;
+    }
+    int find_min()
+    {
+        Node* p = first;
+        int min = 1000000;
+        while (p)
+        {
+            p->val < min ? min = p->val : 0;
+            p = p->next;
+        }
+        return min;
+    }
+};
+int main()
+{
+    list l;
+    int size;
+    cout << "Enter size of linked list: ";
+    cin >> size;
+    for (int i = 0; i < size; i++)
+    {
+        int tmp;
+        cout << "Enter " << i + 1 << " element of list: ";
+        cin >> tmp;
+        l.push_back(tmp);
+    }
+    cout << "Minimal value of linked list is: " << l.find_min();
+    return 0;
+}
